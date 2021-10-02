b405c0edd4a4740e2cfa6a586f2dc4f344507b66
cmezq <cmezq@MACBOOK-PRO>
2021-10-01 23:43:28 -0500

    Agregar archivos de proyecto.

---
+++ IMC/Persona.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IMC
+{
+    public class Persona
+    {
+        public double Peso { get; set; }
+        public double Altura { get; set; }
+    }
+}
